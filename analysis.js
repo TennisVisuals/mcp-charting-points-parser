b3ecb5ef7563439103b02341d310127f0ab5d9f0
TennisVisuals <tennis.aip@gmail.com>
2016-01-14 13:07:27 +0100

    new functions

--- analysis.js
+++ analysis.js
@@ -5,8 +5,66 @@ module.exports = function() {
 
    // Analysis
 
+   analyze.playerMatches = playerMatches;
+   function playerMatches(match_array, player_name) {
+      var matches = [];
+      match_array.forEach(function(match) {
+         var players = match.match.players();
+         if (players[0].search(player_name) >= 0 || players[1].search(player_name) >= 0) {
+            matches.push(match);
+         }
+      });
+      return matches;
+   }
+
+   analyze.serveAnalysis = serveAnalysis;
+   function serveAnalysis(points, verbose) {
+      var serve_types = {};
+      var invalid_serves = 0;
+      for (var p=0; p < points.length; p++) {
+         var point = points[p];
+         if (point.serves.length == 0) invalid_serves += 1;
+         if (point.first_serve && point.first_serve.serves && point.first_serve.serves.length == 0) invalid_serves += 1;
+         if (point.serves.length == 1) {
+            if (!serve_types[point.serves[0]]) {
+               serve_types[point.serves[0]] = 1;
+            } else {
+               serve_types[point.serves[0]] += 1;
+            }
+         }
+      }
+      var analysis = {
+         serve_types: serve_types,
+         invalid_serves: invalid_serves
+      };
+      return analysis
+   }
+
+   analyze.matchesServeAnalysis = matchesServeAnalysis;
+   function matchesServeAnalysis(matches, verbose) {
+      var serve_types = {};
+      var invalid_serves = 0;
+      matches.forEach(function(match, i) {
+         var analysis = serveAnalysis(match.match.points(), verbose);
+         invalid_serves += analysis.invalid_serves;
+         var serve_keys = Object.keys(analysis.serve_types);
+         serve_keys.forEach(function(s) {
+            if (!serve_types[s]) {
+               serve_types[s] = analysis.serve_types[s];
+            } else {
+               serve_types[s] += analysis.serve_types[s];
+            }
+         });
+      });
+      var matches_analysis = {
+         serve_types: serve_types,
+         invalid_serves: invalid_serves
+      };
+      return matches_analysis
+   }
+
    analyze.rallyDepth = rallyDepth;
-   function rallyDepth(points) {
+   function rallyDepth(points, verbose) {
       var no_depth_or_finish = [];
       var rally_count = 0;
       var return_count = 0;
@@ -37,7 +95,7 @@ module.exports = function() {
                   var rally_depth = findDepth(shots[r]);
                   if (rally_depth) {
                      depth_count += 1;
-                     console.log(points[p].serves, shots);
+                     if (verbose) console.log(points[p].serves, shots);
                   }
                }
             }
@@ -70,17 +128,17 @@ module.exports = function() {
       return analysis;
    }
 
-   analyze.matchRallies = matchRallies;
-   function matchRallies(matches) {
+   analyze.matchesWithRallyDepth = matchesWithRallyDepth;
+   function matchesWithRallyDepth(matches, verbose) {
       var rally_shots = 0;
       var rally_depth = 0;
       var matches_with_rally_depth = [];
       matches.forEach(function(match, i) {
-         var analysis = rallyDepth(match.match.points());
+         var analysis = rallyDepth(match.match.points(), verbose);
          rally_shots += analysis.shots;
          if (analysis.rally_depth > 0) {
             matches_with_rally_depth.push(i);
-            rally_depth += 1;
+            rally_depth += analysis.rally_depth;
          }
       });
       return { rally_shots: rally_shots, rally_depth: rally_depth, matches: matches_with_rally_depth };
