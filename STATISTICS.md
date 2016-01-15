6ca60988c8b8de14c0f6706cf2c819062d6b990e
TennisVisuals <tennis.aip@gmail.com>
2016-01-15 07:34:55 +0100

    additions

--- STATISTICS.md
+++ STATISTICS.md
@@ -6,9 +6,8 @@ The statistics package is a work in progress.
 Please view the README file for details on installing the mcp-charting-points-parser.  These notes assume familiarity.
 
 **counters()** reduces raw point data into numbers from which statistics can be calculated.
-
 ```
-> p.st.counters(match.points())
+> c = p.st.counters(match.points())
 { FirstServes: [ 59, 56 ],
   SecondServes: [ 20, 18 ],
   Points: [ 53, 62 ],
@@ -23,4 +22,17 @@ Please view the README file for details on installing the mcp-charting-points-pa
   BreakpointConversions: [ , 3 ],
   Ace: [ 6, 4 ],
   DoubleFaults: [ 2 ] }
+```
+**baseStats()** converts counters into Overview stats for an arbitrary collection of points.  Use the *sets* accessor on the match object to retrieve points for a single set.
+```
+> p.st.baseStats(c)
+{ '0': { AcePct: '10.17' }, '1': { AcePct: '7.14' } }
+...
+
+> s1 = p.st.counters(match.sets()[0].points())
+...
+
+> p.st.baseStats(s1)
+{ '0': { AcePct: '3.33' }, '1': { AcePct: '10.00' } }
+...
 ```
--- statistics.js
+++ statistics.js
@@ -14,7 +14,7 @@ module.exports = function() {
          increment('Points', point.winner);
          if (point.server != point.winner) increment('ReturnPointsWon', 1 - point.server);
 
-         if (point.result == 'Ace') increment('Ace', point.server);
+         if (point.result == 'Ace') increment('Aces', point.server);
          if (point.result == 'Serve Winner') increment('ServeWinners', point.server);
          if (point.result == 'Double Fault') increment('DoubleFaults', point.server);
 
@@ -41,6 +41,24 @@ module.exports = function() {
             stat_obj[what][who] = 1;
          }
       }
+   }
+
+   statistics.baseStats = baseStats;
+   function baseStats(counters) {
+      var player_stats = { 0: {}, 1: {} };
+      for (var p=0; p < 2; p++) {
+         player_stats[p].AcePct = (counters.Aces && counters.FirstServes) ?
+                                  cpct(counters.Aces[p], counters.FirstServes[p]) : undefined;
+         player_stats[p].DFPct  = (counters.DoubleFaults && counters.FirstServes) ?
+                                  cpct(counters.DoubleFaults[p], counters.FirstServes[p]) : undefined;
+      }
+      return player_stats;
+
+      function cpct(count, total) { 
+         if (!total || !count) return 0;
+         return (count / total * 100).toFixed(2); 
+      }
+
    }
 
    return statistics;
